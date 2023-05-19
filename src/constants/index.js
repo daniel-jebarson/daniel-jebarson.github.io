bd328cacd80133f641e8c882bbef088740e41600
daniel-jebarson <danieljebarson21@gmail.com>
2023-05-19 14:03:00 +0530

    💚 Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,119 @@
+export const projects = [
+  {
+    title: "Plants vs Zombies API",
+    desc: "An API built with with NextTS and Tailwind CSS to help website builders build their websites about Plants-vs-Zombies-2 easily! Data has been scraped from fandom websites and all data are  error free",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109025615442608168/pvz.png",
+    link: "https://pvz-2-api.vercel.app/",
+    code: "https://github.com/code-cracked/plants-vs-zombies-api",
+  },
+  {
+    title: "Web-Chat-App",
+    desc: "An app made with NextJs to help each other communicate fastly and securely. We can also search for the friends with their username.Realtime updates on messages.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "ChakraUI",
+        color: "green-600",
+      },
+      {
+        name: "SocketIO",
+        color: "yellow-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "https://user-images.githubusercontent.com/88134306/227576773-06c3a1d2-0a1a-4ab2-8d1f-e06d71408410.png",
+    link: "https://web-chat-app-brown.vercel.app",
+    code: "https://github.com/daniel-jebarson/web-chat-app",
+  },
+  {
+    title: "Exploiter (discord bot - deprecated)",
+    desc: "This is the simple multi-featured bot in discord that resembles lords mobile as mini game. It contains the featiures of many bot such as dyno,carl-bot,mee6.With the help of this we can authorize the users, audit the messages and can also be used for server developments.  ",
+    tags: [
+      {
+        name: "Python",
+        color: "red-600",
+      },
+      {
+        name: "Discord.py",
+        color: "green-600",
+      },
+      {
+        name: "discord-interactions",
+        color: "yellow-600",
+      },
+      {
+        name: "Flask",
+        color: "blue-800",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109031291522928670/Exploiter.png",
+    link: "https://drive.google.com/file/d/1npEOuOhQh-1zRiLB9fFn-6ORTwd1gZkl/view",
+    code: "https://github.com/daniel-jebarson/EXPLOITER",
+  },
+  {
+    title: "Stock Market",
+    desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109032447783469097/image.png",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/daniel-jebarson/Stock-Market",
+  },
+  {
+    title: "You-Farm",
+    desc: "An app made with NextJs, Machine Learning and ExpressJs to help farmers to sell their crops without intermediate. It also suggests which crops are needed for the specific soil.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "MaterialUI",
+        color: "green-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "yellow-600",
+      },
+      {
+        name: "FireBase",
+        color: "blue-800",
+      },
+      {
+        name: "Machine Learning",
+        color: "orange-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109035277814611978/Screenshot_19.png",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+];
