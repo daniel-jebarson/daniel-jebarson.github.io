597d32ef8bdc2a3e3dc20dc1e0ac5c6a394b2eea
daniel-jebarson <danieljebarson21@gmail.com>
2023-05-20 14:19:33 +0530

    ✨ Add email service

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,8 +7,66 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
 
 export default function Contact() {
+  const [mailSent, setMailSent] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, email, message } = e.target.elements;
+    const { REACT_APP_SERVICEID, REACT_APP_TEMPLATE, REACT_APP_PUBLICKEY } =
+      process.env;
+    console.log({
+      username: username.value,
+      email: email.value,
+      message: message.value,
+    });
+    const templateParams = {
+      username: username.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    emailjs
+      .send(
+        REACT_APP_SERVICEID,
+        REACT_APP_TEMPLATE,
+        templateParams,
+        REACT_APP_PUBLICKEY
+      )
+      .then(
+        (response) => {
+          setMailSent(true);
+          toast.success("Message Sent Successfully!", {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          toast.error("Error occured!", {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+          console.log("FAILED...", err);
+        }
+      );
+  };
   return (
     <div
       id="contact"
@@ -17,47 +75,68 @@ export default function Contact() {
       <div className="text-5xl font-bold">Contact</div>
       <div className="flex flex-row  ">
         <div className="flex-auto  w-32 pl-10">
-          <label class="block mb-2 text-2xl font-semibold mt-3  text-white ">
-            Get in Touch
-          </label>
-          <hr className="border-cyan-700 border-2 w-1/3 mx-auto bg-cyan-700" />
-          <div class="flex pt-9">
-            <span class="inline-flex items-center px-3 text-sm  border border-r-0  rounded-l-md bg-gray-600 text-gray-400 border-gray-600">
-              <PersonIcon />
-            </span>
-            <input
-              type="text"
-              id="website-admin"
-              class="rounded-none rounded-r-lg  border block flex-1 min-w-0 w-full text-sm p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Name"
-            />
-          </div>
-          <div class="flex pt-9">
-            <span class="inline-flex items-center px-3 text-sm border border-r-0 rounded-l-md bg-gray-600 text-gray-400 border-gray-600">
-              <AlternateEmailIcon />
-            </span>
-            <input
-              type="text"
-              id="website-admin"
-              class="rounded-none rounded-r-lg border block flex-1 min-w-0 w-full text-sm p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Email"
-            />
-          </div>
-          <textarea
-            id="message"
-            rows="4"
-            placeholder="Write your message here..."
-            class="block h-40 text-white mt-10 p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 focus:ring-blue-500 focus:border-blue-500 resize-none"
-          ></textarea>
-          <button className="bg-clr_blue  mt-7 py-2 px-4 rounded-md hover:bg-clr_btn_hover">
-            Send Message <EmailIcon />
-          </button>
-          <button
-            disable={"true"}
-            className="bg-green-700 cursor-default hidden  mt-7 py-2 px-4 rounded-md "
-          >
-            Message Sent <FileDownloadDoneIcon />
-          </button>
+          <form onSubmit={handleSubmit}>
+            <label class="block mb-2 text-2xl font-semibold mt-3  text-white ">
+              Get in Touch
+            </label>
+            <hr className="border-cyan-700 border-2 w-1/3 mx-auto bg-cyan-700" />
+            <div class="flex pt-9">
+              <span class="inline-flex items-center px-3 text-sm  border border-r-0  rounded-l-md bg-gray-600 text-gray-400 border-gray-600">
+                <PersonIcon />
+              </span>
+              <input
+                disabled={mailSent}
+                readOnly={mailSent}
+                id="username"
+                required
+                type="text"
+                class="rounded-none rounded-r-lg  border block flex-1 min-w-0 w-full text-sm p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Name"
+              />
+            </div>
+            <div class="flex pt-9">
+              <span class="inline-flex items-center px-3 text-sm border border-r-0 rounded-l-md bg-gray-600 text-gray-400 border-gray-600">
+                <AlternateEmailIcon />
+              </span>
+              <input
+                disabled={mailSent}
+                readOnly={mailSent}
+                id="email"
+                required
+                minLength={5}
+                type="email"
+                class="rounded-none rounded-r-lg border block flex-1 min-w-0 w-full text-sm p-2.5  bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Email"
+              />
+            </div>
+            <textarea
+              id="message"
+              disabled={mailSent}
+              readOnly={mailSent}
+              rows="4"
+              required
+              minLength={10}
+              placeholder="Write your message here..."
+              class="block h-40 text-white mt-10 p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 focus:ring-blue-500 focus:border-blue-500 resize-none"
+            ></textarea>
+            <button
+              type="submit"
+              className={`bg-clr_blue ${
+                mailSent === false ? "" : "hidden"
+              }  mt-7 py-2 px-4 rounded-md hover:bg-clr_btn_hover`}
+            >
+              Send Message <EmailIcon />
+            </button>
+            <button
+              type="button"
+              disable={"true"}
+              className={`bg-green-700 ${
+                mailSent === true ? "visible" : "hidden"
+              } cursor-default  mt-7 py-2 px-4 rounded-md `}
+            >
+              Message Sent <FileDownloadDoneIcon />
+            </button>
+          </form>
         </div>
         <div className="flex-auto w-80  flex flex-col text-left justify-center   gap-7">
           <div class="max-w-sm p-6  border rounded-lg shadow bg-gray-800 mx-auto border-gray-700 hover:border-gray-600 ">
@@ -128,6 +207,18 @@ export default function Contact() {
             </div>
           </div>
         </div>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
       </div>
     </div>
   );
