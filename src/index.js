5d0c16eb7c32245d4185193fa30867fa88d7f74b
daniel-jebarson <danieljebarson21@gmail.com>
2023-05-25 10:23:54 +0530

    💫 Add : Analytics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics
+      beforeSend={(event) => {
+        // Ignore all events that have a `/private` inside the URL
+        if (event.url.includes("localhost")) {
+          return null;
+        }
+        return event;
+      }}
+    />
   </React.StrictMode>
 );
 
